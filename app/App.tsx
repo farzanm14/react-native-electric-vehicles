fa2fec124ca45e2ea303ebcd1a6482b4bdbc18f8
Farzaneh Mirzakhanloo <farzaneh.mirzakhanloo@gmail.com>
2023-08-12 13:15:44 +0330

    init new project

---
+++ app/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>New react native project</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
