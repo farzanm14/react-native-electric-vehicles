a48001d438aae5d15bb5e7805d2ebe78f618e4e2
Farzaneh Mirzakhanloo <farzaneh.mirzakhanloo@gmail.com>
2023-08-15 23:33:53 +0330

    config testing-library

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-  preset: 'react-native',
+  preset: "react-native",
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  globals: {
+    "ts-jest": {
+      babelConfig: "./babel.config.js",
+      diagnostics: false,
+    },
+  },
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+    "^.+\\.tsx?$": "ts-jest",
+    "\\.js$": "<rootDir>/node_modules/babel-jest",
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!(jest-)?react-native|@react-native-community|@react-navigation)",
+  ],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    "^.+\\.(css|less|scss)$": "identity-obj-proxy",
+  },
+  moduleDirectories: ["node_modules", "src"],
 };
